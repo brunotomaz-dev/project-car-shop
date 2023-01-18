0feea61a2d5271c8f975f58f071ee68ed05b1acf
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-01-18 18:36:42 -0300

    realizando mais testes

--- tests/unit/Services/car.test.ts
+++ tests/unit/Services/car.test.ts
@@ -3,7 +3,14 @@ import { Model } from 'mongoose';
 import Sinon from 'sinon';
 import Car from '../../../src/Domains/Car';
 import CarsService from '../../../src/Services/CarsService';
-import { inputCarData, outputCarData } from '../mocks/carsMock';
+import HttpException from '../../../src/utils/httpException';
+import {
+  arrayCarData,
+  atualizarCarData,
+  inputCarData,
+  inputCarDataInvalid,
+  outputCarData,
+} from '../mocks/carsMock';
 
 describe('Teste da camada service de car', function () {
   it('Deve criar um carro em Service', async function () {
@@ -14,7 +21,50 @@ describe('Teste da camada service de car', function () {
     const car = await carService.create(inputCarData);
 
     expect(car).to.be.deep.equal(carOutput);
+  });
 
+  it('Deve retornar um carro pelo id', async function () {
+    const carOutput = new Car(outputCarData);
+    Sinon.stub(Model, 'findById')
+      .withArgs(outputCarData.id)
+      .resolves(carOutput);
+
+    const carService = new CarsService();
+    const car = await carService.find(outputCarData.id);
+
+    expect(car).to.be.deep.equal(carOutput);
+  });
+
+  it('Deve retornar todos os carros', async function () {
+    Sinon.stub(Model, 'find').resolves(arrayCarData);
+
+    const carService = new CarsService();
+    const car = await carService.findAll();
+
+    expect(car).to.be.deep.equal(arrayCarData);
+  });
+
+  it('Deve atualizar um carro pelo id', async function () {
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(atualizarCarData);
+
+    const carService = new CarsService();
+    const car = await carService.update(outputCarData.id, atualizarCarData);
+
+    expect(car).to.be.deep.equal(atualizarCarData);
+  });
+
+  it('Deve lançar erro ao tentar atualizar um carro com id inválido', async function () {
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(false);
+
+    try {
+      const carService = new CarsService();
+      await carService.update(inputCarDataInvalid.id, atualizarCarData);
+    } catch (error) {
+      expect((error as HttpException).message).to.be.deep.equal('Invalid mongo id');
+    }
+  });
+
+  afterEach(function () {
     Sinon.restore();
   });
 });
--- tests/unit/mocks/carsMock.ts
+++ tests/unit/mocks/carsMock.ts
@@ -18,3 +18,48 @@ export const outputCarData = {
   doorsQty: 4,
   seatsQty: 5,
 };
+
+export const atualizarCarData = {
+  id: '63c5f0285f3ba1c06a48d865',
+  model: 'Uno',
+  color: 'White',
+  year: 1995,
+  buyValue: 10.99,
+  status: false,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export const inputCarDataInvalid = {
+  id: 'invalid id',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export const arrayCarData = [
+  {
+    id: '63c5f0285f3ba1c06a48d865',
+    model: 'Marea',
+    color: 'Black',
+    year: 2002,
+    buyValue: 15.99,
+    status: true,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '63c5f0285f3ba1c06a48d865',
+    model: 'Uno',
+    color: 'White',
+    year: 1995,
+    buyValue: 10.99,
+    status: false,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+];
